72384d759bc769f5431ce3b85f7278db4960e6d6
Rawser Spicer <rwspicer@alaska.edu>
2017-09-22 15:53:18 -0800

    removed extra barrow code from run_general

--- atm/run_general.py
+++ atm/run_general.py
@@ -44,48 +44,6 @@ import set_lake_pond_depth
 import set_pond_growth_array
 import set_protective_layer
 
-#=====================================================================
-def initialize_barrow(self):
-    initial_cohort_population.barrow_initial_cohort_population(self)
-    initial_cohort_check.barrow_initial_cohort_check(self)
-    cohort_present.barrow_cohort_present(self)
-#---------------------------------------------------------------------
-def initialize_barrow_cohorts(self):
-    print '=================================== '
-    print ' Initializing Lake & Pond Properties'
-    print '===================================='
-    initialize.LakePond(self)
-    set_lake_pond_depth.set_lake_pond_depth(self)
-    set_lake_ice_depth_constant.set_lake_ice_depth_constant(self)
-    set_ice_thickness_array.set_ice_thickness_array(self)
-    climate_expansion_arrays.set_climate_expansion_arrays(self)
-    set_pond_growth_array.set_pond_growth_array(self)
-    print '====================================='
-    print ' Initializing Terrestrial Properties'
-    print '====================================='
-    initialize.Terrestrial_Barrow(self)
-    read_ice_content.read_ice_content(self)
-    read_drainage_efficiency.read_drainage_efficiency(self)
-    read_initial_ALD.read_initial_ALD(self)
-    set_ALD_constant.set_ALD_constant(self)
-    set_ALD_array.set_ALD_array(self)
-    set_protective_layer.set_protective_layer(self)
-    set_initial_cumulative_probability.set_initial_cumulative_probability(self)
-    # Initializing Terrestrial Cohort Properties
-    initialize.CLC_WT(self)
-    initialize.CoastalWaters_WT(self)
-    initialize.DrainedSlope_WT(self)
-    initialize.FCP_WT(self)
-    initialize.HCP_WT(self)
-    initialize.LCP_WT(self)
-    initialize.Meadow_WT(self)
-    initialize.NoData_WT(self)
-    initialize.SandDunes_WT(self)
-    initialize.SaturatedBarrens_WT(self)
-    initialize.Shrubs_WT(self)
-    initialize.Urban_WT(self)
-    initial_cohort_age.initial_cohort_age(self)
-#---------------------------------------------------------------------
 
 barrow = [ 
     'lake_pond_expansion', 'pond_infill', 
