ecf050f0a180d6c443835f3f592569c1325ddc27
Rawser Spicer <rwspicer@alaska.edu>
2017-10-30 09:54:08 -0800

    start of tool

---
+++ atm/tools/generate_raster.py
@@ -0,0 +1,53 @@
+"""
+Generate Raster
+---------------
+
+tools for generating atm input rasters
+"""
+from osgeo import ogr
+
+
+ACP_metadata = {
+    'NoData': 0,
+    'Coalescent low-center polygons': 2,
+    'Low-center polygons': 3,
+    'Meadow': 4,
+    'Drained slope': 7,
+    'Flat-center polygons': 8,
+    'High-center polygons': 9,
+    'Sandy Barrens': 10,
+    'Sand Dunes':  11,
+    'Ice': 12,
+    'Saline coastal water': 13,
+    'Rivers': 16,
+    'Urban': 17,
+    'Riparian shrub': 18,
+    'Ponds': 20,
+    'Small Lakes': 21,
+    'Medium Lakes': 22,
+    'Large Lakes': 23,
+}
+
+
+def from_shapefile(infile, metadata):
+    """
+    """
+    filter_attr = '"GRIDCODE"'
+    
+    shapefile = ogr.Open(infile)
+    layer = shapefile.GetLayer(0)
+    extent = layer.GetExtent()
+    
+    for land_cover in metadata:
+        val = metadata[ land_cover ]
+        layer.SetAttributeFilter( filter_attr + ' = ' + str(val) )
+        cmd = 'gdal_rasterize -a Area '
+        cmd += '-tr 25 25 -te '
+        cmd += 563500.0 7868000.0 631500.0 7927000.0 -a_nodata -9999 '
+        exec(cmd)
+        
+    
+    
+    
+    
+    
