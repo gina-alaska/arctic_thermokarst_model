fddaec77fc7194cc1f40bd287d86a5493ed2ba79
Rawser Spicer <rwspicer@alaska.edu>
2017-11-08 10:38:40 -0900

    added drainage grid

---
+++ atm/grids/drainage_grid.py
@@ -0,0 +1,165 @@
+"""
+Drainage Grid
+-------------
+
+Grid for drainage efficiency
+"""
+import os
+import numpy as np
+import pickle
+
+config_ex = {
+    'Terrestrial_Control': {
+        'Drainage_Efficiency_Distribution': 'random',
+        'Drainage_Efficiency_Random_Value': 	0.85,
+        'Drainage_Efficiency_Figure':		'Yes' ,
+    },
+    'pickle path': './pickles',
+    'shape': (10,10),
+    'AOI mask': np.random.choice([True,False],(10,10))
+}
+
+class DrainageTypeInvalid(Exception):
+    """Raised if lake/pond type not found"""
+
+class DrainageGrid (object):
+    """ DrainageGrid """
+    
+    def __init__ (self, config):
+        """Drainage Efficiency Grid 
+        
+        Parameters
+        ----------
+        config: dict or atm.control or path to pickle file
+            configuration for object
+            
+        Attributes
+        ----------
+        shape: tuple
+            shape of grid
+        grid: np.array
+            drainage grid
+        pickle_path: path
+            path to pickle file
+        """
+        if type(config) is str:
+            ## read from existing pickle
+            self.load_from_pickle(config)
+            return
+            
+        self.shape = config['shape']
+        aoi = config['AOI mask']
+        eff = config['Terrestrial_Control']['Drainage_Efficiency_Distribution']
+        threshold = config['Terrestrial_Control']\
+            ['Drainage_Efficiency_Random_Value']
+        
+        
+        self.grid = self.setup(eff, self.shape, threshold, aoi)
+        self.pickle_path = os.path.join(
+            config['pickle path'], 'drainage_grid.pkl'
+        )
+
+    def setup (self, efficiency, shape, threshold = .5, aoi = None):
+        """setup grid
+        
+        Parameters
+        ----------
+        efficiency: str
+            ['above', 'below', 'random']
+        shape: tuple
+            (rows, columns)
+        threshold: float, defaults .5
+            when randomizing values > threshold become above, <= below
+        aoi: np.array, optional
+            area of interest mask
+            grid cells that are in AOI == True, False other wise
+        
+        Returns
+        -------
+        np.array:
+            flattened drainage grid with values ['above', 'below', 'none']
+        """
+        efficiency = efficiency.lower()
+        valid_input = ['above', 'below', 'random']
+        if not efficiency in valid_input:
+            msg = 'Efficiency Type not in ' + str(valid_input)
+            raise DrainageTypeInvalid, msg
+        
+        grid = np.random.random(shape).flatten()
+        
+        if efficiency == 'random':
+            grid[grid > threshold] = 1
+            grid[grid <= threshold] = 0
+            
+            grid = grid.astype(int).astype(str)
+            grid[grid == '1'] = 'above'
+            grid[grid == '0'] = 'below'
+            
+            
+        else:
+            grid = grid.astype(str)
+            grid[:] = efficiency
+            
+        if not aoi is None:
+            grid[ aoi.flatten() == False ] = 'none'
+            
+        return grid
+        
+    def get_grid (self, flat = True):
+        """gets the gird
+        
+        Parameters
+        ----------
+        flat: bool, Default True
+            if False, retruned grid is reshaped to shape
+        
+        Returns
+        -------
+        np.array
+            the grid
+        """
+        shape = self.shape[0]*self.shape[1] if flat == True else self.shape
+        return self.grid.reshape(shape)
+        
+
+    def load_from_pickle(self, pickle_name):
+        """load state from pickle file. sets shape, and grid to values
+        in file, and pickle_path to pickle_name
+    
+        Parameters
+        ----------
+        pickel_name: path
+            path to pickle file
+        """
+        with open(pickle_name, 'rb') as pkl:
+            data = pickle.load(pkl)
+            
+        self.shape = data['shape']
+        self.grid = data['grid']
+        self.pickle_path = pickle_name
+        
+    def write_to_pickle (self, pickle_name = None):
+        """Write to pickle, object are serialized as a python dictionary 
+        with shape, and grid as keys
+        
+        Parameters
+        ----------
+        pickle_name: path, optional
+            path to write to.
+        """
+        if pickle_name is None:
+            pickle_name = self.pickle_path
+        
+        data = {
+            'shape': self.shape,
+            'grid': self.grid
+        }    
+        
+        mode = 'wb'
+
+        with open(pickle_name, mode) as pkl:
+            pickle.dump(data, pkl)
+        
+    def figure (self):
+        """TODO: save figures"""
+        raise NotImplementedError, "need to write"
---
+++ tests/test_drainage_grid.py
@@ -0,0 +1,62 @@
+"""test_ice_grid.py
+"""
+
+import unittest
+from context import atm
+from atm.grids import drainage_grid
+
+import os
+import numpy as np
+
+class TestDrainageGridClass(unittest.TestCase):
+    """test the DrainageGrid class"""
+    def setUp(self):
+        """setup class for tests 
+        """
+        config = drainage_grid.config_ex
+        
+        config['pickle path'] = 'test_pickles'
+        try:
+            os.makedirs(config['pickle path'])
+        except:
+            pass
+        
+        self.drainage = drainage_grid.DrainageGrid(config)
+    
+    def tearDown(self):
+        """
+        """
+        try:
+            pass
+            os.remove(self.drainage.pickle_path)
+            os.removedirs('test_pickles')
+        except:
+            pass
+            
+    def test_init (self):
+        """ Function doc """
+        self.assertEqual( (10,10), self.drainage.shape )
+                
+        grid = self.drainage.get_grid()
+        grid[grid == 'none'] = 0 
+        grid[grid == 'above'] = 1
+        grid[grid == 'below'] = 2
+        self.assertTrue( 
+            grid.astype(int).all() in [0,1,2]
+        )
+        
+        
+    def test_get_item (self):
+        """ Function doc """
+        self.assertEqual( (10*10,), self.drainage.get_grid().shape )
+        self.assertEqual( (10,10), self.drainage.get_grid(False).shape )
+        
+    def test_load_pickle (self):
+        """ Function doc """
+        self.drainage.write_to_pickle()
+        
+        new = drainage_grid.DrainageGrid(self.drainage.pickle_path)
+        self.assertTrue((new.get_grid() == self.drainage.get_grid()).all())
+
+if __name__ == '__main__':
+    unittest.main()
