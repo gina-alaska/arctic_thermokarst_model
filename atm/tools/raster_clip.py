c8e4f8044bf15a6ed26bd49fa18feb7fcae4d866
Rawser Spicer <rwspicer@alaska.edu>
2017-11-15 13:39:58 -0900

    added raster tools

--- atm/tools/raster_clip.py
+++ atm/tools/raster_clip.py
@@ -1,6 +1,6 @@
 
 
-from atm.io import raster
+from atm.atm_io import raster
 import gdal
 import os
 import numpy as np
@@ -24,21 +24,4 @@ def clip_rasters (in_dir, out_dir, extent, ext = '.tif'):
         clip_raster(in_path, out_path, extent)
 
 
-def load_and_stack (in_dir, ext = '.tif'):
-    """
-    """
-    files = [f for f in os.listdir(in_dir) if f[-4:] == ext]
-    for fdx in range(len(files)):
-        f = files[fdx]
-        print f
-        ras = os.path.join(in_dir,f)
-        
-        if fdx ==0:
-            data = raster.load_raster(ras)[0].flatten()
-        else:
-            data = np.vstack((data,  raster.load_raster(ras)[0].flatten()))
-            print 'a'
-       
-            
-        print data.shape
-    return data
+
---
+++ atm/tools/stack_rasters.py
@@ -0,0 +1,45 @@
+
+from atm.atm_io import raster
+import numpy as np
+
+def load_and_stack (files):
+    """
+    """
+   
+    for fdx in range(len(files)):
+        
+        f = files[fdx]
+        
+        if fdx == 0:
+            data, md = raster.load_raster(f)
+            shape = data.shape
+            data = data.flatten()
+        else:
+            data = np.vstack((data,  raster.load_raster(f)[0].flatten()))
+       
+            
+        #~ print data.shape
+    return data, shape
+
+
+def load_and_stack_memory_mapped (files, filename = 'temp.data'):
+    """ 
+    """
+    for fdx in range(len(files)):
+        f = files[fdx]
+        
+        if fdx == 0:
+           
+            r, md = raster.load_raster(f)
+            
+            shape = (len(files), r.shape[0] * r.shape[1])
+            data = np.memmap(filename, dtype='float32', mode='w+', shape=shape)
+            
+            shape = r.shape
+            data[0] = r.flatten()
+        else:
+            data[fdx] = raster.load_raster(f)[0].flatten()))
+       
+            
+        #~ print data.shape
+    return data, shape
