d0441970ff487fd53d8f745da7492134bcfdc596
Rawser Spicer <rwspicer@alaska.edu>
2017-11-18 14:50:11 -0900

    added tools to use as cli

--- atm/tools/generate_raster.py
+++ atm/tools/generate_raster.py
@@ -31,6 +31,18 @@ sample_metadata = {
     'Large Lakes': 23,
 }
 
+
+# dtype map
+str_to_gdal = {
+    'float': gdal.GDT_Float32,
+    #~ 'float32': gdal.GDT_Float32,
+    'float64': gdal.GDT_Float64,
+    'int': gdal.GDT_Int32, 
+    'byte': gdal.GDT_Byte,
+
+}
+
+
 def create_empty_raster ( 
         out_raster, extent, pixel_size, num_layers = 1, dtype = gdal.GDT_Byte
     ):
@@ -73,7 +85,7 @@ def create_empty_raster (
 
 def from_shapefile(
         in_shape, out_raster, metadata,
-        log = False, single = False, out_path = './', dtype = gdal.GDT_Byte
+        log = False, single = False, out_path = './', dtype = 'byte'
     ):
     """convert shape file to raster files(file)
     
@@ -113,15 +125,16 @@ def from_shapefile(
     layer = source.GetLayer(0)
     extent = layer.GetExtent()
     #~ print extent
-    
-    
-    
 
     if single:
         if len(out_raster.split('.')) == 1:
             out_raster += '.tif'
         dest = create_empty_raster(
-            os.path.join(out_path,out_raster), extent, pixel_size, num_layers = len(metadata)
+            os.path.join(out_path,out_raster),
+            extent,
+            pixel_size,
+            num_layers = len(metadata),
+            dtype = str_to_gdal[dtype]
         )
     
     
@@ -136,7 +149,12 @@ def from_shapefile(
             dest = None
             out = \
                 out_raster.split('.')[0] + "_" + attr.replace(' ','_') + '.tif'
-            dest = create_empty_raster(os.path.join(out_path,out), extent, pixel_size)
+            dest = create_empty_raster(
+                os.path.join(out_path,out),
+                extent,
+                pixel_size,
+                dtype = str_to_gdal[dtype]
+            )
         
         val = metadata[attr]
         layer.SetAttributeFilter( filter_attr + ' = ' + str(val) )
@@ -181,6 +199,83 @@ def parse_metadata(text):
     return metadata
 
 
-
+def utility ():
+    """
+    Utility to generate rastes from shape file attributes
     
+    Flags
+    -----
+    '--in_shapefile': path
+        path to shapefile
+    '--out_name': str
+        name or file or file tag
+    '--metadata': path
+        path to metadta file. File should contain key of attrbute names
+        and values equal to shape file attributes
+    '--out_path': path, defaluts to cwd
+        path to save rasters in
+    '--single_raster': bool, defaults False
+        if true a single raster with multiple names is generated
+    '--type': str
+        'float', 'float64', 'int', or 'byte'
+    """
+    try:
+        from utilitools import clite 
+        try:
+            arguments = clite.CLIte(
+                ['--in_shapefile', '--out_name', '--metadata', '--out_path'],
+                ['--single_raster', '--type']
+            
+            )
+        except (clite.CLIteHelpRequestedError, clite.CLIteMandatoryError):
+            print utility.__doc__
+            return
+        
+        shapefile = arguments['--in_shapefile']
+        raster = arguments['--out_name']
+        metadata = arguments['--metadata']
+        out_path = arguments['--out_path']
+        single = bool(arguments['--single_raster'])
+        if single is None:
+            single = False
+        dtype = arguments['--type']
+        if dtype is None:
+            dtype = 'float'
+        
+        
+    except ImportError:
+        import sys
+        print ("utility usage: python generate_raster.py"
+               " shapefile raster_name metadata_file out_path"
+        )
+        shapefile = sys.argv[1]
+        raster = sys.argv[2]
+        metadata = sys.argv[3]
+        out_path = sys.argv[4]
+        single = False
+        dtype = 'float'
+    return
+    #~ print shapefile, raster, metadata, out_path, single, dtype
+
+    with open(metadata, 'r') as meta:
+        text = meta.read()
+        metadata = parse_metadata(text)
+        
+    try:
+        os.mkdir(out_path)
+    except OSError:
+        pass
+    
+    from_shapefile(
+        shapefile,
+        raster,
+        metadata,
+        log = True,
+        single = single,
+        dtype = dtype,
+        out_path = out_path
+    )
+        
+if __name__ == '__main__':
+    utility()
     
