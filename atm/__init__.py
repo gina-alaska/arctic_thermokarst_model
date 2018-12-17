f6f5513bea3be92e8de55a8700b99fddc1e0c5d4
rwspicer <rwspicer@alaska.edu>
2018-12-17 11:28:51 -0900

    added trigger tool, work toward installable pkg

--- atm/__init__.py
+++ atm/__init__.py
@@ -1,5 +1,5 @@
 __author__     = "Bob Bolton"
-__copyright__  = "Copyright 2014, 2015, 2016, 2017, Bob Bolton"
+__copyright__  = "Copyright 2014, 2015, 2016, 2017, 2018 Bob Bolton"
 __credits__    = [
     "Bob Bolton",
     "Vladimir Romanovsky",
@@ -8,10 +8,10 @@ __credits__    = [
     "Rawser Spicer", 
     "Jen Delamere",
 ]
-__license__    = "GPL"
+__license__    = "MIT"
 __version__    = "0.4.3"
 __codeurl__    = \
-    'https://github.com/gina-alaska/arctic_thermokarst_model/tree/0.4.2'
+    'https://github.com/ua-snap/arctic_thermokarst_model'
 __maintainer__ = "Bob Bolton"
-__email__      = "bbolton@iarc.uaf.edu"
+__email__      = "wrbolton@alaska.edu"
 __status__     = "Development"
