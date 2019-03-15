3a650d6f3149cd5d5a930b1495bbad2b9245c152
rwspicer <rwspicer@alaska.edu>
2019-03-15 10:38:23 -0800

    update metadata

--- atm/__init__.py
+++ atm/__init__.py
@@ -9,9 +9,9 @@ __credits__    = [
     "Jen Delamere",
 ]
 __license__    = "GPL"
-__version__    = "0.4.3"
+__version__    = "0.5.0-dev"
 __codeurl__    = \
-    'https://github.com/gina-alaska/arctic_thermokarst_model/tree/0.4.2'
+    'https://github.com/gina-alaska/arctic_thermokarst_model/tree/tree/'+__version__
 __maintainer__ = "Bob Bolton"
 __email__      = "bbolton@iarc.uaf.edu"
 __status__     = "Development"
