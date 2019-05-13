bd40546fb3b961d850964d029ba622baad264afe
rwspicer <rwspicer@alaska.edu>
2019-05-13 12:53:07 -0800

    added clip generation to temporal multigrids, and used in reenable cohort results clips

--- atm/ATM.py
+++ atm/ATM.py
@@ -387,8 +387,7 @@ class ATM(object):
         for control in sorted(self.control['cohorts']):
             if type(self.control['cohorts'][control]) is str:
                 continue
-            if not self.control['cohorts'][control]['Figures']:
-                continue
+            
             
             cohort = '_'.join(control.split('_')[:-1])
             path = os.path.join(outdir, cohort, 'year_cohorts')
@@ -396,7 +395,7 @@ class ATM(object):
                 os.makedirs(path)
             except:
                 pass
-            vid = self.control['cohorts'][control]['Movie']
+            
             self.logger.add( "    -- " + cohort + " Figures/Video")
             fig_args = {
                 'title': '',
@@ -406,19 +405,53 @@ class ATM(object):
                 'vmax': 1,
                 }
 
+            frames = []
+            if self.control['cohorts'][control]['Figures']:
+                for year in range(start_year, start_year + self.stop-1):
+                    fig_args['title'] = \
+                        DISPLAY_COHORT_NAMES[cohort] +' - ' + str(year)
+                    fname = \
+                        os.path.join(
+                            path, 
+                            cohort + '_Fractional_Area_'+ str(year)+'.png'
+                        )
+                    self.grids.area.save_figure(
+                        cohort, year, fname, figures.default, fig_args
+                    )
+                    frames.append(fname)
+            
+            clip_args = {
+                'figure_args': fig_args,
+            }
+            if self.control['cohorts'][control]['Movie']:
+                if frames != []:
+                    clip_args['frames list'] = frames
+                    # print frames
+                    path = os.path.join(outdir, cohort)
+                    complete = self.grids.area.save_clip(
+                        None,
+                        os.path.join(path, cohort + '_Fractional_Area.mp4'),
+                        clip_args=clip_args
+                    )
+                else:
+                    path = os.path.join(outdir, cohort)
+                    clip_args['end_ts'] = self.stop
+                    complete = self.grids.area.save_clip(
+                        cohort,
+                        os.path.join(path, cohort + '_Fractional_Area.mp4'),
+                        clip_args=clip_args
+                    )
+                    
 
-            for year in range(start_year, start_year + self.stop-1):
-                fig_args['title'] = \
-                    DISPLAY_COHORT_NAMES[cohort] +' - ' + str(year)
-                self.grids.area.save_figure(
-                    cohort, year, 
-                    os.path.join(path, cohort + '_Fractional_Area_'+ str(year)),
-                    figures.default, 
-                    fig_args
-                )
-                
+                if complete:
+                    self.logger.add(
+                        "       -- " + cohort + " Clip output success"
+                    )
+                else:
+                    self.logger.add(
+                        "       -- " + cohort + " Clip output failed"
+                    )
 
-            # self.grids.area.save_cohort_timeseries(cohort, path, vid)
 
 
             
